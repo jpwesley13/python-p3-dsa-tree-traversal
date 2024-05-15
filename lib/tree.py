3c0decfd48e0f4380554b427b2cf03596313b859
Jared Patterson <jpwesley34@gmail.com>
2024-05-15 16:29:24 -0700

    Passes tests.

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,14 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+
+    nodes_to_visit = [self.root]
+
+    while nodes_to_visit:
+      current = nodes_to_visit.pop(0)
+      if current['id'] == id:
+        return current
+      else:
+        nodes_to_visit = current['children'] + nodes_to_visit
+    
+    return None
